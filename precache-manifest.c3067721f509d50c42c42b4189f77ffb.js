dcfaf7e7137401084ee282d4b6f123d17a01a837
Sabrina Li <sabrinali0312@gmail.com>
2019-06-24 16:28:51 -0400

    Updates

---
+++ precache-manifest.c3067721f509d50c42c42b4189f77ffb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec71ac1a8ac3bb5ee0757ac7757bbd2f",
+    "url": "/clicky-game-react/index.html"
+  },
+  {
+    "revision": "02d90630bc90c62cb6c3",
+    "url": "/clicky-game-react/static/css/main.7156f44a.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/clicky-game-react/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "02d90630bc90c62cb6c3",
+    "url": "/clicky-game-react/static/js/main.7825f36d.chunk.js"
+  },
+  {
+    "revision": "96e9a9b550dae971e8fe",
+    "url": "/clicky-game-react/static/js/runtime~main.4ef103c0.js"
+  }
+]);
